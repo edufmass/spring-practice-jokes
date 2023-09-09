1a31ea2a35dc8956bb0d359404001e27807353a0
edufmass <git@edufmass.ar>
2023-09-08 21:36:28 -0300

    SpringInitialzr template project

---
+++ src/main/java/ar/net/edufmass/springjokes/SpringjokesApplication.java
@@ -0,0 +1,13 @@
+package ar.net.edufmass.springjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/net/edufmass/springjokes/SpringjokesApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.net.edufmass.springjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
